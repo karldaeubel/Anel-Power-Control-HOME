98255c3a8387e9632f4da939e3130709fb7f067f
Karl Daeubel <karl_daeubel@web.de>
2013-01-19 18:42:05 +0100

    Hintergrundbild eingefügt

--- Anel Power/gen/de/power/R.java
+++ Anel Power/gen/de/power/R.java
@@ -14,8 +14,9 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int setting=0x7f020001;
+        public static final int background=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int setting=0x7f020002;
     }
     public static final class id {
         public static final int ScrollView1=0x7f060013;
